6d064144e66e4215f33543207b64d18db90163ac
Vladimir.Silkachev <provovak@yandex.ru>
2019-09-19 16:03:06 +0800

    Initial commit

---
+++ case.turtle.py
@@ -0,0 +1,51 @@
+#Hello guys
+#Developers V.Silkachev
+            #K.Popov
+            #S.Vinnikov
+
+import turtle
+
+def blade1():
+    #TODO: (vova):  левый синий треуголльник
+    pass
+
+def blade2():
+    #TODO: (vova): правый красный паралледограм
+    pass
+
+def triangle1():
+    #TODO: (Kirill): правый большой
+    pass
+
+def triangle2():
+    #TODO: (Kirill): левый большой
+    pass
+
+def rocket():
+    #TODO: (Kirill): трапеция
+    pass
+
+def tail_triangle1():
+   #TODO: (Vinn) правый начало
+    pass
+
+def tail_triangle2():
+   #TODO: (Vinn) левый начало
+    pass
+
+def tail():
+   #TODO: (Vinn) квадрат
+    pass
+def main():
+    blade1()
+    blade2()
+    triangle1()
+    triangle2()
+    rocket()
+    tail_triangle1()
+    tail_triangle2()
+    tail()
+    pass
+
+main()
+turtle.done()
