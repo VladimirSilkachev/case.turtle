1bfa62d62aba9da39176870fa3427a60b1ebc71c
KirillPopov001 <55420132+KirillPopov001@users.noreply.github.com>
2019-09-23 20:41:06 +0700

    кирик

--- case.turtle.py
+++ case.turtle.py
@@ -14,15 +14,48 @@ def blade2():
     pass
 
 def triangle1():
-    #TODO: (Kirill): правый большой
+    #TODO: (Kirill): левый большой
+    turtle.up()
+    turtle.color('yellow')
+    turtle.begin_fill()
+    turtle.right(90)
+    turtle.setposition(k,o)
+    turtle.down()
+    turtle.forward(107)
+    turtle.right(135)
+    turtle.forward(150)
+    turtle.right(135)
+    turtle.forward(107)
+    turtle.end_fill()
     pass
 
 def triangle2():
-    #TODO: (Kirill): левый большой
+     #TODO: (Kirill): правый большой
+    turtle.color("red")
+    turtle.begin_fill()
+    turtle.up()
+    turtle.setposition(l,f)
+    turtle.down()
+    turtle.right(90)
+    turtle.forward(107)
+    turtle.right(270)
+    turtle.forward(107)
+    turtle.left(135)
+    turtle.fd(150)
+    turtle.end_fill()
     pass
 
 def rocket():
-    #TODO: (Kirill): трапеция
+    #TODO: (Kirill): праллелограм
+    turtle.color("pink")
+    turtle.begin_fill()
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(45)
+    turtle.forward(30)
+    turtle.left(135)
+    turtle.forward(50)
+    turtle.end_fill()
     pass
 
 def tail_triangle1(z,x):
@@ -62,15 +95,16 @@ def tail(a,b):
    turtle.right(90)
    turtle.forward(50)
 pass
+
 def main():
-    blade1()
-    blade2()
-    triangle1()
-    triangle2()
-    rocket()
     tail_triangle1()
     tail_triangle2()
     tail()
+    triangle1(125,-18)
+    triangle2(201,-92)
+    rocket()
+    blade1()
+    blade2()
 pass
 
 main()
