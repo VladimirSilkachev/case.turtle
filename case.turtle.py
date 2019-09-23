685ba0807d30b4d39caa01adfdc14d031d818370
Vladimir.Silkachev <provovak@yandex.ru>
2019-09-23 22:22:29 +0800

    end version

--- case.turtle.py
+++ case.turtle.py
@@ -1,51 +1,137 @@
-#Hello guys
-#Developers V.Silkachev
-            #K.Popov
-            #S.Vinnikov
 
 import turtle
-
-def blade1():
-    #TODO: (vova):  левый синий треуголльник
-    pass
+import math
+def blade1(t,r):
+    turtle.up
+    turtle.color('green')
+    turtle.begin_fill()
+    turtle.up()
+    turtle.setposition(t,r)
+    turtle.down()
+    turtle.left(180)
+    turtle.fd(100)
+    turtle.left(45)
+    turtle.fd(70)
+    turtle.left(135)
+    turtle.fd(100)
+    turtle.left(45)
+    turtle.fd(70)
+    turtle.end_fill()
+pass
 
 def blade2():
-    #TODO: (vova): правый красный паралледограм
-    pass
+    turtle.color('blue')
+    turtle.begin_fill()
+    turtle.right(45)
+    turtle.fd(100)
+    turtle.right(135)
+    turtle.fd(73)
+    turtle.right(90)
+    turtle.fd(73)
+    turtle.end_fill()
+pass
 
-def triangle1():
-    #TODO: (Kirill): правый большой
-    pass
+def triangle1(k,o):
+    turtle.up()
+    turtle.color('yellow')
+    turtle.begin_fill()
+    turtle.right(90)
+    turtle.setposition(k,o)
+    turtle.down()
+    turtle.forward(107)
+    turtle.right(135)
+    turtle.forward(150)
+    turtle.right(135)
+    turtle.forward(107)
+    turtle.end_fill()
+pass
 
-def triangle2():
-    #TODO: (Kirill): левый большой
-    pass
+def triangle2(l,f):
+    turtle.color("red")
+    turtle.up()
+    turtle.begin_fill()
+    turtle.setposition(l,f)
+    turtle.down()
+    turtle.right(90)
+    turtle.forward(107)
+    turtle.right(270)
+    turtle.forward(107)
+    turtle.left(135)
+    turtle.fd(150)
+    turtle.end_fill()
+pass
 
 def rocket():
-    #TODO: (Kirill): трапеция
-    pass
+    turtle.color("gray")
+    turtle.begin_fill()
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(45)
+    turtle.forward(30)
+    turtle.left(135)
+    turtle.forward(50)
+    turtle.end_fill()
+pass
 
-def tail_triangle1():
-   #TODO: (Vinn) правый начало
-    pass
+def tail_triangle1(z,x):
+    turtle.color('purple')
+    turtle.begin_fill()
+    turtle.up()
+    turtle.setpos(z,x)
+    turtle.down()
+    turtle.right(45)
+    turtle.forward(50)
+    turtle.right(135)
+    turtle.forward(math.sqrt(5000))
+    turtle.right(135)
+    turtle.forward(50)
+    turtle.end_fill()
+pass
 
-def tail_triangle2():
-   #TODO: (Vinn) левый начало
-    pass
+def tail_triangle2(c,d):
+    turtle.color('pink')
+    turtle.begin_fill()
+    turtle.up()
+    turtle.setpos(c,d)
+    turtle.down()
+    turtle.right(45)
+    turtle.forward(math.sqrt(5000))
+    turtle.right(135)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.end_fill()
+pass
 
-def tail():
-   #TODO: (Vinn) квадрат
-    pass
+def tail(a,b):
+    turtle.color('orange')
+    turtle.begin_fill()
+    turtle.right(45)
+    turtle.up()
+    turtle.setpos(a,b)
+    turtle.down()
+    turtle.right(45)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.end_fill()
+pass
 def main():
-    blade1()
-    blade2()
-    triangle1()
-    triangle2()
+    tail_triangle1(125, -18)
+    tail_triangle2(54, -18)
+    tail(0, 0)
+    triangle1(125,-18)
+    triangle2(201,-92)
     rocket()
-    tail_triangle1()
-    tail_triangle2()
-    tail()
-    pass
+    blade1(201,58)
+    blade2()
+
+pass
 
 main()
 turtle.done()
+
